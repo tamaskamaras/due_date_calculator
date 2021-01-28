d7c9fd03be032f7856c0a5a8f6f2444ab2b37877
Tamás Kamarás <kamaras.tamas.viktor@gmail.com>
2021-01-28 13:08:47 +0100

    formulate #submitted_at expectations

--- spec/due_date_calculator_spec.rb
+++ spec/due_date_calculator_spec.rb
@@ -1,12 +1,46 @@
 require 'rspec'
 
 RSpec.describe DueDateCalculator do
+  subject do
+    described_class.new(
+      submitted_at: submitted_at,
+      turnaround_time: turnaround_time
+    )
+  end
+
   describe '#submitted_at' do
+    let(:turnaround_time) { 16 }
+
     describe 'allows input format as' do
-      context 'Date'
-      context 'Time'
-      context 'DateTime'
-      context 'String'
+      RSpec.shared_examples 'coerce #submitted_at into DateTime' do
+        it 'returns DateTime' do
+          expect(subject.submitted_at).to be_a(DateTime)
+        end
+      end
+
+      context 'Date' do
+        let(:submitted_at) { Date.today }
+
+        include_examples 'coerce #submitted_at into DateTime'
+      end
+
+      context 'Time' do
+        let(:submitted_at) { Time.now }
+
+        include_examples 'coerce #submitted_at into DateTime'
+      end
+
+      context 'DateTime' do
+        let(:submitted_at) { DateTime.now }
+
+        include_examples 'coerce #submitted_at into DateTime'
+      end
+
+      context 'String' do
+        let(:submitted_at) { '2021-01-28' }
+
+        include_examples 'coerce #submitted_at into DateTime'
+      end
     end
 
     describe 'is coerced into working hours' do
@@ -24,4 +58,6 @@ RSpec.describe DueDateCalculator do
       context '80 hours'
     end
   end
+
+  describe '.run'
 end
